45121598e56bf8d84701a4844b33aa85f4fe1d5e
RyanAugust <rad19206@yahoo.com>
2024-01-28 21:01:52 -0800

    init tests for config build and simulation

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,4 +1,8 @@
-from PySiMMMulator import load_parameters
+from pysimmmulator import load_parameters
 
 def test_load_cfg():
     assert type(load_parameters.cfg) == dict
+
+def test_cfg_base_keys():
+    cfg = load_parameters.cfg
+    assert 'basic_params' in cfg.keys()
---
+++ tests/test_sim.py
@@ -0,0 +1,4 @@
+from pysimmmulator import simmmulate, load_parameters
+
+def test_initiate_sim():
+    simmmulate.simulate(load_parameters.my_basic_params)
