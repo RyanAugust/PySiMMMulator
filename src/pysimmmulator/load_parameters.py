17a3a039dcb294bb4a881c17aedc2f3db0547c5a
RyanAugust <rad19206@yahoo.com>
2024-01-29 13:55:39 -0800

    migrate calculations from baseline dataclass to module

--- src/pysimmmulator/load_parameters.py
+++ src/pysimmmulator/load_parameters.py
@@ -1,7 +1,7 @@
 import yaml
 from pysimmmulator.helpers import (
     basic_parameters,
-    baseline,
+    baseline_parameters,
     media_parameters
 )
 
@@ -13,10 +13,10 @@ def load_config(config_path: str = 'config.yaml') -> dict:
 
 def build_params(config_dict: dict) -> None:
     my_basic_params = basic_parameters(**config_dict['basic_params'])
-    my_baseline_params = baseline(basic_params=my_basic_params, **config_dict['baseline_parms'])
+    my_baseline_params = baseline_parameters(basic_params=my_basic_params, **config_dict['baseline_parms'])
 
 
 cfg = load_config(config_path = 'config.yaml')
 my_basic_params = basic_parameters(**cfg['basic_params'])
-my_baseline_params = baseline(basic_params=my_basic_params, **cfg['baseline_parms'])
+my_baseline_params = baseline_parameters(basic_params=my_basic_params, **cfg['baseline_parms'])
 my_media_params = media_parameters(**cfg['media'])
