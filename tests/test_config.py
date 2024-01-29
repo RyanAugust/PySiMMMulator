807058b64e1a32587f2f94efc4a5b52877953c7b
RyanAugust <rad19206@yahoo.com>
2024-01-29 10:38:12 -0800

    tests for media_parameters dataclass

--- tests/test_config.py
+++ tests/test_config.py
@@ -6,3 +6,9 @@ def test_load_cfg():
 def test_cfg_base_keys():
     cfg = load_parameters.cfg
     assert 'basic_params' in cfg.keys()
+
+def test_media_cfg_check():
+    cfg = load_parameters.cfg
+    media_params = load_parameters.media_parameters(**cfg['media'])
+    media_params.check(basic_params=load_parameters.my_basic_params)
+    assert True == True
