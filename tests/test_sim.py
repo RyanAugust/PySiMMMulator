d1dc93d3356850b72348207fc0e413f4035a480c
RyanAugust <rad19206@yahoo.com>
2024-02-07 20:34:13 -0800

    test support for conversions

--- tests/test_sim.py
+++ tests/test_sim.py
@@ -33,6 +33,15 @@ def tests_step5_adstock():
     sim.simulate_cvr(**load_parameters.cfg["cvr_params"])
     sim.simulate_decay_returns(**load_parameters.cfg["adstock_params"])
 
+def tests_step6_conversions():
+    sim = simmmulate.simulate(load_parameters.my_basic_params)
+    sim.simulate_baseline(**load_parameters.cfg["baseline_params"])
+    sim.simulate_ad_spend(**load_parameters.cfg["ad_spend_params"])
+    sim.simulate_media(**load_parameters.cfg["media_params"])
+    sim.simulate_cvr(**load_parameters.cfg["cvr_params"])
+    sim.simulate_decay_returns(**load_parameters.cfg["adstock_params"])
+    sim.calculate_conversions()
+
 def test_run_with_config():
     sim = simmmulate.simulate(load_parameters.my_basic_params)
     sim.run_with_config()
