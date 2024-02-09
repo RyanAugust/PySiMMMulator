c0e894330b94a448f9979281b63cb239c6fb55f4
RyanAugust <rad19206@yahoo.com>
2024-02-08 21:24:50 -0800

    single point version declaration

--- setup.py
+++ setup.py
@@ -1,3 +1,9 @@
 from setuptools import setup
+import os
 
-setup()
+with open(os.path.join('src/cheetahpy', 'VERSION')) as version_file:
+    version = version_file.read().strip()
+
+setup(
+    version=version
+)
---
+++ src/pysimmmulator/__init__.py
@@ -0,0 +1,8 @@
+__title__ = 'CheetahPy'
+__author__ = 'RyanAugust'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2024'
+
+import os
+with open(os.path.join('src/cheetahpy', 'VERSION')) as version_file:
+    __version__ = version_file.read().strip()
