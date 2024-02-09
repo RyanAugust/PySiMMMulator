5ea6e21b59e5a1037d9b2f5e20231383dbee83a0
RyanAugust <rad19206@yahoo.com>
2024-02-08 21:31:23 -0800

    correct dir

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 import os
 
-with open(os.path.join('src/cheetahpy', 'VERSION')) as version_file:
+with open(os.path.join('src/pysimmmulator', 'VERSION')) as version_file:
     version = version_file.read().strip()
 
 setup(
