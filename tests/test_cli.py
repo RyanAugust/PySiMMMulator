db993b5aba29f3ffebcb84047364590e5a26b174
RyanAugust <rad19206@yahoo.com>
2024-02-14 19:10:44 -0800

    added support for output specification

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,4 +1,7 @@
 from pysimmmulator import simmmulate, command_line
+import subprocess
+
 
 def test_cli():
+    subprocess.run(["pysimmm", "-c", "example_config.yaml", "-o", "./"])
     assert 1 == 1
