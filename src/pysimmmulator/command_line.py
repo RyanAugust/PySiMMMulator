2885f1be32a1c798846c6dc76306ad19f5e018a5
Ryan Duecker <ryan.duecker@yahoo.com>
2024-02-22 11:56:09 -0500

    Update arguments to `run_with_config`

--- src/pysimmmulator/command_line.py
+++ src/pysimmmulator/command_line.py
@@ -1,9 +1,10 @@
 from pysimmmulator import load_config, simmmulate
 import pandas as pd
 import argparse
+import os
 
 
-def run_with_config(config_path):
+def run_with_config(config_path, output_path):
     """Initiates `simmmulator` and executes `simmmmulator.run_with_config` against the passed config filepath
 
     Args:
@@ -14,8 +15,8 @@ def run_with_config(config_path):
     (mmm_input_df, channel_roi) = sim.run_with_config(config=cfg)
 
     # save to current directory. Should be an optional argument for this
-    mmm_input_df.to_csv("mmm_input_df.csv", index=False)
-    pd.DataFrame.from_dict(channel_roi, orient="index", columns=["true_roi"]).to_csv("channel_roi.csv")
+    mmm_input_df.to_csv(os.path.join(output_path,"mmm_input_df.csv"), index=False)
+    pd.DataFrame.from_dict(channel_roi, orient="index", columns=["true_roi"]).to_csv(os.path.join(output_path,"channel_roi.csv"))
 
 
 def main():
@@ -27,4 +28,4 @@ def main():
         "-o", "--output_path", action="store", help="Provides output destination", default="."
     )
     args = arg_parser.parse_args()
-    run_with_config(config_path=args.config_path)
+    run_with_config(config_path=args.input_config, output_path=args.output_path)
