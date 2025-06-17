ebf2869dad20309ab9034b1ed9e88d8ac9f24c95
Ryan Duecker <ryan.duecker@gmail.com>
2025-06-17 14:55:03 -0400

    redesign version tracking

--- src/pysimmmulator/__init__.py
+++ src/pysimmmulator/__init__.py
@@ -2,12 +2,9 @@ __title__ = "PySiMMM"
 __author__ = "RyanAugust"
 __license__ = "MIT"
 __copyright__ = "Copyright 2024"
+__version__ = "0.4.2"
 
 import os
-
-with open(os.path.join(os.path.dirname(__file__), "VERSION")) as version_file:
-    __version__ = version_file.read().strip()
-
 
 from .simulate import simmm, multisimmm
 from .load_parameters import load_config, define_basic_params
