5583493ef09a8a94a6165c07de485309fd8dbafb
Ryan Duecker <ryan.duecker@gmail.com>
2025-11-06 11:51:26 -0500

    Add methods for generation of synthetic study results

--- src/pysimmmulator/__init__.py
+++ src/pysimmmulator/__init__.py
@@ -9,3 +9,4 @@ import os
 from .simulate import simmm, multisimmm
 from .load_parameters import load_config, define_basic_params
 from .geos import geos, distribute_to_geos
+from .study import Study, BatchStudy
